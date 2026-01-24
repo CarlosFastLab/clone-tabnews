3b2bc1eaa6cb8a91cb898cc7805bc6d8d7181b6c
Carlos Fast <fastcaf@hotmail.com>
2026-01-24 03:05:05 -0300

    Refactoring fix

--- tests/integration/api/v1/migrations/all.test.js
+++ tests/integration/api/v1/migrations/all.test.js
@@ -9,10 +9,20 @@ async function cleanDatabase() {
 test("/api/v1/migrations should early return 405 if unexpected methods are used", async () => {
   const unavailableMethods = ["PATCH", "PUT", "DELETE", "HEAD", "OPTIONS"];
 
-  unavailableMethods.forEach(async (method) => {
+  for (const method of unavailableMethods) {
     const response = await fetch("http://localhost:3000/api/v1/migrations", {
       method,
     });
     expect(response.status).toBe(405);
-  });
+  }
+
+  const statusEndpointResponse = await fetch(
+    "http://localhost:3000/api/v1/status",
+  );
+
+  const statusEndpointResponseBody = await statusEndpointResponse.json();
+
+  expect(
+    statusEndpointResponseBody.dependencies.database.opened_connections,
+  ).toBeLessThanOrEqual(1);
 });
